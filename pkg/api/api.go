f0b19779d29abb7565d106a6187780012d83ede2
Elena <elenatur03@outlook.com>
2026-04-15 21:10:54 +0300

    Add auth

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -3,8 +3,10 @@ package api
 import "net/http"
 
 func Init() {
-	http.HandleFunc("/api/nextdate", NextDateHandler)
-	http.HandleFunc("/api/task", taskHandler)
-	http.HandleFunc("/api/tasks", tasksHandler)
-	http.HandleFunc("/api/task/done", doneTaskHandler)
+	http.HandleFunc("/api/signin", signinHandler)
+
+	http.HandleFunc("/api/nextdate", auth(NextDateHandler))
+	http.HandleFunc("/api/task", auth(taskHandler))
+	http.HandleFunc("/api/tasks", auth(tasksHandler))
+	http.HandleFunc("/api/task/done", auth(doneTaskHandler))
 }
--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = ``
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NzYzMDUxMDYsInBhc3NfaGFzaCI6IjU5OTQ0NzFhYmIwMTExMmFmY2MxODE1OWY2Y2M3NGI0ZjUxMWI5OTgwNmRhNTliM2NhZjVhOWMxNzNjYWNmYzUifQ.yAyJ-q5Liml7JS_dtoIuLLIopQrgSnJncG3HP6apoTA`
